91f286962f55a062f954b316ac458bc3ef7839db
Alexei <alexei.artsimovich@gmail.com>
2020-01-25 18:10:06 +0300

    refactored FileDeletion class

--- app/src/main/java/com/frolo/muse/di/impl/local/FileDeletion.java
+++ app/src/main/java/com/frolo/muse/di/impl/local/FileDeletion.java
@@ -2,15 +2,14 @@ package com.frolo.muse.di.impl.local;
 
 import java.io.File;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 
 /**
  * Dispatcher center:
- * the clients may listen to file deletion via {@link FileDeletion#startWatching(Watcher)}
- * and stop listening via {@link FileDeletion#stopWatching(Watcher)}
+ * the clients may listen to a file deletion via {@link FileDeletion#startWatching(Watcher)}
+ * and stop listening via {@link FileDeletion#stopWatching(Watcher)}.
  *
  * Dispatching is done via {@link FileDeletion#dispatchDeleted(List)} method.
  */
@@ -20,19 +19,19 @@ final class FileDeletion {
         void onFilesDeleted(List<File> files);
     }
 
-    private static final Collection<Watcher> WATCHERS =
+    private static final Collection<Watcher> sWatchers =
             new ConcurrentLinkedQueue<>();
 
     static void startWatching(Watcher w) {
-        WATCHERS.add(w);
+        sWatchers.add(w);
     }
 
     static void stopWatching(Watcher w) {
-        WATCHERS.remove(w);
+        sWatchers.remove(w);
     }
 
     static void dispatchDeleted(List<File> files) {
-        for (Watcher w : WATCHERS) {
+        for (Watcher w : sWatchers) {
             w.onFilesDeleted(files);
         }
     }
