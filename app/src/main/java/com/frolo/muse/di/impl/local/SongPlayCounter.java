97f6ade3e0b1c19f3f0b586620fd7a2ca7702b1f
Alexei <alexei.artsimovich@gmail.com>
2020-01-25 18:13:34 +0300

    refactored SongPlayCounter class

--- app/src/main/java/com/frolo/muse/di/impl/local/SongPlayCounter.java
+++ app/src/main/java/com/frolo/muse/di/impl/local/SongPlayCounter.java
@@ -6,10 +6,10 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * This is a dispatcher center that notifies registered {@link Watcher}s
- * that play count has been changed for a song.
+ * that the play counter of a song has been changed.
  *
- * A {@link Watcher} is registered by {@link SongPlayCounter#startWatching(Watcher)} method and
- * unregistered by {@link SongPlayCounter#stopWatching(Watcher)} method.
+ * A {@link Watcher} may be registered by {@link SongPlayCounter#startWatching(Watcher)} method
+ * and may be unregistered by {@link SongPlayCounter#stopWatching(Watcher)} method.
  *
  * Dispatching is done via {@link SongPlayCounter#dispatchChanged(String)} method.
  */
@@ -17,9 +17,8 @@ final class SongPlayCounter {
 
     interface Watcher {
         /**
-         * Called when play count has been changed for a song
-         * that is stored on <code>absolutePath</code>.
-         * @param absolutePath of a song that play count has been changed for
+         * Called when play count has been changed for a song that is stored on <code>absolutePath</code>.
+         * @param absolutePath of a song whose play counter has been changed.
          */
         void onChanged(String absolutePath);
     }
@@ -27,15 +26,15 @@ final class SongPlayCounter {
     private static final Collection<Watcher> sWatchers =
             new ConcurrentLinkedQueue<>();
 
-    synchronized static void startWatching(Watcher w) {
+    static void startWatching(Watcher w) {
         sWatchers.add(w);
     }
 
-    synchronized static void stopWatching(Watcher w) {
+    static void stopWatching(Watcher w) {
         sWatchers.remove(w);
     }
 
-    synchronized static void dispatchChanged(String absolutePath) {
+    static void dispatchChanged(String absolutePath) {
         for (Watcher w : sWatchers) {
             w.onChanged(absolutePath);
         }
