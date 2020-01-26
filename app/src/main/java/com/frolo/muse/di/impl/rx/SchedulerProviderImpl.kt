83561654cd2decf8964cb995c019245c61fcfab6
Alexei <alexei.artsimovich@gmail.com>
2020-01-26 13:33:50 +0300

    changed worker scheduler in scheduler provider implementation

--- app/src/main/java/com/frolo/muse/di/impl/rx/SchedulerProviderImpl.kt
+++ app/src/main/java/com/frolo/muse/di/impl/rx/SchedulerProviderImpl.kt
@@ -4,14 +4,11 @@ import com.frolo.muse.rx.SchedulerProvider
 import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-import java.util.concurrent.Executors
 
 
 class SchedulerProviderImpl : SchedulerProvider {
-    private val workerExecutor = Executors.newFixedThreadPool(3)
-    private val workerScheduler = Schedulers.from(workerExecutor)
 
-    override fun worker(): Scheduler = workerScheduler
+    override fun worker(): Scheduler = Schedulers.io()
 
     override fun computation(): Scheduler = Schedulers.computation()
 
