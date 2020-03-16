94a1f6634b529257d40d41db36f034f9f64af39e
Alexei <alexei.artsimovich@gmail.com>
2020-03-16 22:08:40 +0300

    refactored PlayButton, optimized setState method

--- mediabutton/src/main/java/com/frolo/mediabutton/PlayButton.java
+++ mediabutton/src/main/java/com/frolo/mediabutton/PlayButton.java
@@ -1,11 +1,11 @@
 package com.frolo.mediabutton;
 
-
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat;
 import androidx.core.content.ContextCompat;
 import android.util.AttributeSet;
+
 
 public class PlayButton extends androidx.appcompat.widget.AppCompatImageView {
     private Drawable pause;
@@ -15,7 +15,7 @@ public class PlayButton extends androidx.appcompat.widget.AppCompatImageView {
 
     private State state = State.PAUSE;
 
-    private AnimatedVectorDrawableCompat animated;
+    private AnimatedVectorDrawableCompat animatedVector;
 
     public PlayButton(Context context) {
         this(context, null);
@@ -30,12 +30,6 @@ public class PlayButton extends androidx.appcompat.widget.AppCompatImageView {
         init(context, attrs);
     }
 
-    private void cancelAnimated() {
-        if (animated != null) {
-            animated.stop();
-        }
-    }
-
     private void init(Context context, AttributeSet attrs) {
         resumeToPause = AnimatedVectorDrawableCompat.create(context, R.drawable.ic_resume_to_pause);
         pauseToResume = AnimatedVectorDrawableCompat.create(context, R.drawable.ic_pause_to_resume);
@@ -48,28 +42,33 @@ public class PlayButton extends androidx.appcompat.widget.AppCompatImageView {
     }
 
     public void setState(State state, boolean animate) {
-        this.state = state;
-        checkStateInternal(state, animate);
+        if (this.state != state) {
+            this.state = state;
+            checkStateInternal(state, animate);
+        }
     }
 
     private synchronized void checkStateInternal(State state, boolean animate) {
-        cancelAnimated();
+        if (animatedVector != null) {
+            animatedVector.stop();
+        }
+
         if (state == State.PAUSE) {
             if (animate && isAttachedToWindow()) {
-                animated = resumeToPause;
-                setImageDrawable(animated);
-                animated.start();
+                animatedVector = resumeToPause;
+                setImageDrawable(animatedVector);
+                animatedVector.start();
             } else {
-                animated = null;
+                animatedVector = null;
                 setImageDrawable(pause);
             }
         } else {
             if (animate && isAttachedToWindow()) {
-                animated = pauseToResume;
-                setImageDrawable(animated);
-                animated.start();
+                animatedVector = pauseToResume;
+                setImageDrawable(animatedVector);
+                animatedVector.start();
             } else {
-                animated = null;
+                animatedVector = null;
                 setImageDrawable(resume);
             }
         }
