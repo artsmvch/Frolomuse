2e2825119157997f7ab28971533daf268d6bd771
Alexei <alexei.artsimovich@gmail.com>
2020-04-06 12:58:14 +0300

    refactored AppRecyclerView class

--- app/src/main/java/com/frolo/muse/views/spring/AppRecyclerView.kt
+++ app/src/main/java/com/frolo/muse/views/spring/AppRecyclerView.kt
@@ -1,17 +1,22 @@
 package com.frolo.muse.views.spring
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.util.AttributeSet
+import androidx.core.view.updatePadding
 import androidx.recyclerview.R
 import com.l4digital.fastscroll.FastScrollRecyclerView
+import com.l4digital.fastscroll.FastScroller
 
 
 /**
  * Same as [SpringRecyclerView], but in addition the fast scroll is implemented.
  */
-class SpringFastScrollRecyclerView @JvmOverloads constructor(
-    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = R.attr.recyclerViewStyle
+class AppRecyclerView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = R.attr.recyclerViewStyle
 ) : FastScrollRecyclerView(context, attrs, defStyleAttr) {
 
     private val springManager = SpringEdgeEffect.Manager(this)
@@ -19,6 +24,16 @@ class SpringFastScrollRecyclerView @JvmOverloads constructor(
     private var shouldTranslateSelf = true
 
     private var isTopFadingEdgeEnabled = true
+
+    private val fastScroller: FastScroller? by lazy {
+        try {
+            val field = FastScrollRecyclerView::class.java.getDeclaredField("fastScroller")
+            field.isAccessible = true
+            field.get(this) as FastScroller
+        } catch (ignored: Throwable) {
+            null
+        }
+    }
 
     init {
         edgeEffectFactory = springManager.createFactory()
@@ -41,5 +56,20 @@ class SpringFastScrollRecyclerView @JvmOverloads constructor(
 
     override fun getTopFadingEdgeStrength(): Float {
         return if (isTopFadingEdgeEnabled) super.getTopFadingEdgeStrength() else 0f
+    }
+
+    override fun setClipToPadding(clipToPadding: Boolean) {
+        super.setClipToPadding(clipToPadding)
+        fastScroller?.clipToPadding = clipToPadding
+    }
+
+    override fun setPadding(left: Int, top: Int, right: Int, bottom: Int) {
+        super.setPadding(left, top, right, bottom)
+        fastScroller?.updatePadding(
+            left = left,
+            top = top,
+            right = right,
+            bottom = bottom
+        )
     }
 }
