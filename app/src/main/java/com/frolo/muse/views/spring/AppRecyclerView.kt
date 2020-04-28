8cb22b998d26c524ce38f51b743bed95db9be08f
Alexei <alexei.artsimovich@gmail.com>
2020-04-28 11:50:21 +0300

    refactored AppRecyclerView widget

--- app/src/main/java/com/frolo/muse/views/spring/AppRecyclerView.kt
+++ app/src/main/java/com/frolo/muse/views/spring/AppRecyclerView.kt
@@ -5,20 +5,20 @@ import android.graphics.Canvas
 import android.util.AttributeSet
 import androidx.appcompat.view.ContextThemeWrapper
 import androidx.core.view.updateLayoutParams
-import androidx.recyclerview.R
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SimpleItemAnimator
+import com.frolo.muse.R
 import com.frolo.muse.StyleUtil
-import com.l4digital.fastscroll.FastScrollRecyclerView
 import com.l4digital.fastscroll.FastScroller
 
 
 /**
- * Same as [SpringRecyclerView], but in addition the fast scroll is implemented.
+ * Customized RecyclerView that implements fast scroll and spring over scroll.
  */
 class AppRecyclerView @JvmOverloads constructor(
     context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.recyclerViewStyle
-) : FastScrollRecyclerView(fastScrollerContext(context), attrs, defStyleAttr) {
+    attrs: AttributeSet? = null
+) : RecyclerView(context, attrs) {
 
     private val springManager = SpringEdgeEffect.Manager(this)
 
@@ -26,19 +26,20 @@ class AppRecyclerView @JvmOverloads constructor(
 
     private var isTopFadingEdgeEnabled = true
 
-    private val fastScroller: FastScroller? by lazy {
-        try {
-            val field = FastScrollRecyclerView::class.java.getDeclaredField("fastScroller")
-            field.isAccessible = true
-            field.get(this) as FastScroller
-        } catch (ignored: Throwable) {
-            null
-        }
-    }
+    private val fastScroller: FastScroller
 
     init {
+        fastScroller = FastScroller(fastScrollerContext(context), attrs)
+        fastScroller.id = com.l4digital.fastscroll.R.id.fast_scroller
+
         edgeEffectFactory = springManager.createFactory()
-        itemAnimator?.setupDurationsByDefault()
+
+        (itemAnimator as? SimpleItemAnimator)?.apply {
+            addDuration = 100
+            removeDuration = 100
+            moveDuration = 150
+            changeDuration = 150
+        }
     }
 
     override fun draw(canvas: Canvas) {
@@ -59,9 +60,33 @@ class AppRecyclerView @JvmOverloads constructor(
         return if (isTopFadingEdgeEnabled) super.getTopFadingEdgeStrength() else 0f
     }
 
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        fastScroller.attachRecyclerView(this)
+    }
+
+    override fun onDetachedFromWindow() {
+        fastScroller.detachRecyclerView()
+        super.onDetachedFromWindow()
+    }
+
+    override fun setAdapter(adapter: Adapter<*>?) {
+        super.setAdapter(adapter)
+        if (adapter is FastScroller.SectionIndexer) {
+            fastScroller.setSectionIndexer(adapter as FastScroller.SectionIndexer?)
+        } else if (adapter == null) {
+            fastScroller.setSectionIndexer(null)
+        }
+    }
+
+    override fun setVisibility(visibility: Int) {
+        super.setVisibility(visibility)
+        fastScroller.visibility = visibility
+    }
+
     override fun setPadding(left: Int, top: Int, right: Int, bottom: Int) {
         super.setPadding(left, top, right, bottom)
-        fastScroller?.updateLayoutParams<MarginLayoutParams> {
+        fastScroller.updateLayoutParams<MarginLayoutParams> {
             topMargin += top
             bottomMargin += bottom
         }
@@ -72,7 +97,8 @@ class AppRecyclerView @JvmOverloads constructor(
         private fun fastScrollerContext(context: Context): Context {
             val fastScrollerThemeId =
                     StyleUtil.readStyleAttrValue(context, com.frolo.muse.R.attr.fastScrollerStyle)
-            return ContextThemeWrapper(context, fastScrollerThemeId)
+            return if (fastScrollerThemeId != 0) ContextThemeWrapper(context, fastScrollerThemeId)
+            else ContextThemeWrapper(context, R.style.Base_AppTheme_FastScroller)
         }
 
     }
