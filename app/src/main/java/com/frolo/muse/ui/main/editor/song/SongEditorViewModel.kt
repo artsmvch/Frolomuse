9d5688d635536b482935856db005aae77ca4d9ef
Alexei <alexei.artsimovich@gmail.com>
2020-05-02 11:01:48 +0300

    minor refactoring in SongEditorViewModel

--- app/src/main/java/com/frolo/muse/ui/main/editor/song/SongEditorViewModel.kt
+++ app/src/main/java/com/frolo/muse/ui/main/editor/song/SongEditorViewModel.kt
@@ -20,58 +20,60 @@ import java.util.concurrent.TimeUnit
 
 
 class SongEditorViewModel constructor(
-        application: App,
-        private val player: Player,
-        private val schedulerProvider: SchedulerProvider,
-        private val repository: SongRepository,
-        private val eventLogger: EventLogger,
-        private val songArg: Song
+    application: App,
+    private val player: Player,
+    private val schedulerProvider: SchedulerProvider,
+    private val repository: SongRepository,
+    private val eventLogger: EventLogger,
+    private val songArg: Song
 ): BaseAndroidViewModel(application, eventLogger) {
 
     private val _isLoadingUpdate: MutableLiveData<Boolean> = MutableLiveData()
-    val isLoadingUpdate: LiveData<Boolean> = _isLoadingUpdate
+    val isLoadingUpdate: LiveData<Boolean> get() = _isLoadingUpdate
 
     private val _updatedSong: MutableLiveData<Song> = MutableLiveData()
-    val updatedSong: LiveData<Song> = _updatedSong
+    val updatedSong: LiveData<Song> get() = _updatedSong
 
     private val _updateError: MutableLiveData<Throwable> = MutableLiveData()
-    val updateError: LiveData<Throwable> = _updateError
+    val updateError: LiveData<Throwable> get() = _updateError
 
     fun onSaveClicked(
-            title: String,
-            album: String,
-            artist: String,
-            genre: String) {
+        title: String,
+        album: String,
+        artist: String,
+        genre: String
+    ) {
 
         Completable
-                // First try updating tags using MyID3 lib
-                .fromAction { updateTagsInternal(songArg, title, album, artist, genre) }
-                .andThen(repository.getSong(songArg.source))
-                // if an error occurred then try updating tags using repository
-                .onErrorResumeNext(repository.update(songArg, title, album, artist, genre))
-                .subscribeOn(schedulerProvider.worker())
-                .observeOn(schedulerProvider.main())
-                .doOnSubscribe { _isLoadingUpdate.value = true }
-                .doFinally { _isLoadingUpdate.value = false }
-                .doOnSuccess { updatedSong -> player.update(updatedSong) }
-                .subscribe { newSong: Song?, err: Throwable? ->
-                    if (newSong != null) {
-                        _updatedSong.value = newSong
-                    } else if (err != null) {
-                        _updateError.value = err
-                    }
+            // First try updating tags using MyID3 lib
+            .fromAction { updateTagsInternal(songArg, title, album, artist, genre) }
+            .andThen(repository.getSong(songArg.source))
+            // if an error occurred then try updating tags using repository
+            .onErrorResumeNext(repository.update(songArg, title, album, artist, genre))
+            .subscribeOn(schedulerProvider.worker())
+            .observeOn(schedulerProvider.main())
+            .doOnSubscribe { _isLoadingUpdate.value = true }
+            .doFinally { _isLoadingUpdate.value = false }
+            .doOnSuccess { updatedSong -> player.update(updatedSong) }
+            .subscribe { newSong: Song?, err: Throwable? ->
+                if (newSong != null) {
+                    _updatedSong.value = newSong
+                } else if (err != null) {
+                    _updateError.value = err
                 }
-                .save()
+            }
+            .save()
     }
 
     @WorkerThread
     @Throws(Exception::class)
     private fun updateTagsInternal(
-            song: Song,
-            newTitle: String,
-            newAlbum: String,
-            newArtist: String,
-            newGenre: String) {
+        song: Song,
+        newTitle: String,
+        newAlbum: String,
+        newArtist: String,
+        newGenre: String
+    ) {
 
         val myID3 = MyID3()
 
@@ -104,4 +106,5 @@ class SongEditorViewModel constructor(
         }
         countDownLatch.await(10, TimeUnit.SECONDS) // awaiting with timeout
     }
+
 }
