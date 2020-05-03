edf35c5226eb0d462b64f6dc051c37f48d6acdda
Alexei <alexei.artsimovich@gmail.com>
2020-05-03 13:27:27 +0300

    updated item layout in SongOfArtistAdapter

--- app/src/main/java/com/frolo/muse/ui/main/library/artists/artist/songs/SongOfArtistAdapter.kt
+++ app/src/main/java/com/frolo/muse/ui/main/library/artists/artist/songs/SongOfArtistAdapter.kt
@@ -1,6 +1,5 @@
 package com.frolo.muse.ui.main.library.artists.artist.songs
 
-import android.view.View
 import android.view.ViewGroup
 import com.bumptech.glide.RequestManager
 import com.frolo.muse.R
@@ -13,6 +12,7 @@ import com.frolo.muse.ui.getNameString
 import com.frolo.muse.ui.main.library.base.SongAdapter
 import com.frolo.muse.views.media.MediaConstraintLayout
 import kotlinx.android.synthetic.main.include_check.view.*
+import kotlinx.android.synthetic.main.include_song_art_container.view.*
 import kotlinx.android.synthetic.main.item_song_of_artist.view.*
 
 
@@ -30,32 +30,32 @@ class SongOfArtistAdapter constructor(
         position: Int,
         item: Song,
         selected: Boolean, selectionChanged: Boolean
-    ) = with(holder.itemView as MediaConstraintLayout) {
-        val res = resources
-        tv_song_name.text = item.getNameString(res)
-        tv_album_name.text = item.getAlbumString(res)
-        tv_duration.text = item.getDurationString()
+    ) {
 
         val isPlayPosition = position == playingPosition
 
-        if (isPlayPosition) {
-            mini_visualizer.visibility = View.VISIBLE
-            mini_visualizer.setAnimate(isPlaying)
-        } else {
-            mini_visualizer.visibility = View.GONE
-            mini_visualizer.setAnimate(false)
+        with(holder.itemView as MediaConstraintLayout) {
+            val res = resources
+            tv_song_name.text = item.getNameString(res)
+            tv_album_name.text = item.getAlbumString(res)
+            tv_duration.text = item.getDurationString()
+
+            requestManager.makeRequest(item.albumId)
+                .placeholder(R.drawable.ic_framed_music_note_48dp)
+                .error(R.drawable.ic_framed_music_note_48dp)
+                .circleCrop()
+                .into(imv_album_art)
+
+            imv_check.setChecked(selected, selectionChanged)
+
+            setChecked(selected)
+            setPlaying(isPlayPosition)
         }
 
-        requestManager.makeRequest(item.albumId)
-            .placeholder(R.drawable.ic_framed_music_note_48dp)
-            .error(R.drawable.ic_framed_music_note_48dp)
-            .circleCrop()
-            .into(imv_album_art)
-
-        imv_check.setChecked(selected, selectionChanged)
-
-        setChecked(selected)
-        setPlaying(isPlayPosition)
+        holder.resolvePlayingPosition(
+            isPlayPosition = isPlayPosition,
+            isPlaying = isPlaying
+        )
     }
 
 }
