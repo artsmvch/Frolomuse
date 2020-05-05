768cf46acb30dd55851176ae472427b7c507aadb
Alexei <alexei.artsimovich@gmail.com>
2020-05-05 22:51:59 +0300

    rounded album shortcut icon, refactored

--- app/src/main/java/com/frolo/muse/di/impl/local/Shortcuts.java
+++ app/src/main/java/com/frolo/muse/di/impl/local/Shortcuts.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.util.TypedValue;
 
 import androidx.annotation.MainThread;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.WorkerThread;
 import androidx.core.content.pm.ShortcutInfoCompat;
 import androidx.core.content.pm.ShortcutManagerCompat;
 import androidx.core.graphics.drawable.IconCompat;
@@ -25,30 +27,41 @@ import com.frolo.muse.model.media.Playlist;
 import com.frolo.muse.model.media.Song;
 import com.frolo.muse.model.media.UnknownMediaException;
 import com.frolo.muse.ui.main.MainActivity;
+import com.frolo.muse.util.BitmapUtil;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.Callable;
 
 import io.reactivex.Completable;
 import io.reactivex.Single;
-import io.reactivex.SingleSource;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.functions.BiFunction;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 
 final class Shortcuts {
     private Shortcuts() {
     }
 
-    /**
-     * Data holder for nullable {@link Bitmap}.
-     * Dummy workaround for RxJava2 that does not allow nullable types.
-     */
-    private static class BitmapResult {
-        @Nullable
-        final Bitmap bitmap;
+    private static class ShortcutParams {
+        @NonNull
+        final String shortcutId;
 
-        BitmapResult(@Nullable Bitmap bitmap) {
-            this.bitmap = bitmap;
+        @NonNull
+        final String shortcutLabel;
+
+        @NonNull
+        final Intent intent;
+
+        @Nullable
+        final Bitmap icon;
+
+        ShortcutParams(@NonNull String shortcutId, @NonNull String shortcutLabel, @NotNull Intent intent, @Nullable Bitmap icon) {
+            this.shortcutId = shortcutId;
+            this.shortcutLabel = shortcutLabel;
+            this.intent = intent;
+            this.icon = icon;
         }
     }
 
@@ -98,9 +111,7 @@ final class Shortcuts {
     @MainThread
     private static void installShortcut_Internal(
         @NonNull Context context,
-        @NonNull Intent shortcutIntent,
-        @NonNull Media media,
-        @Nullable Bitmap icon
+        @NonNull ShortcutParams params
     ) throws Exception {
         ThreadStrictMode.assertMain();
 
@@ -108,78 +119,78 @@ final class Shortcuts {
             throw new IllegalStateException("Pin shortcuts are not supported");
         }
 
-        final String shortcutId = getShortcutId(media);
-
         final ShortcutInfoCompat.Builder shortcutInfoBuilder =
-                new ShortcutInfoCompat.Builder(context, shortcutId);
+                new ShortcutInfoCompat.Builder(context, params.shortcutId);
 
-        if (icon != null) {
-            shortcutInfoBuilder.setIcon(IconCompat.createWithBitmap(icon));
+        if (params.icon != null) {
+            shortcutInfoBuilder.setIcon(IconCompat.createWithBitmap(params.icon));
         } else {
             shortcutInfoBuilder.setIcon(IconCompat.createWithResource(context, R.mipmap.ic_launcher));
         }
 
-        shortcutInfoBuilder.setShortLabel(getShortcutLabel(media));
+        shortcutInfoBuilder.setShortLabel(params.shortcutLabel);
 
-        shortcutInfoBuilder.setIntent(shortcutIntent);
+        shortcutInfoBuilder.setIntent(params.intent);
 
         final ShortcutInfoCompat shortcutInfo = shortcutInfoBuilder.build();
 
         ShortcutManagerCompat.requestPinShortcut(context, shortcutInfo, null);
     }
 
-    private static Single<BitmapResult> getIcon(@NonNull final Context context, @NonNull Media media) {
+    @WorkerThread
+    @Nullable
+    private static Bitmap tryGetShortcutIcon(@NonNull final Context context, @NonNull Media media) {
         if (media.getKind() == Media.SONG) {
-            return Single.defer(new Callable<SingleSource<? extends BitmapResult>>() {
-                @Override
-                public SingleSource<? extends BitmapResult> call() throws Exception {
-                    final Song song = (Song) media;
-                    return getIconForAlbumId(context, song.getAlbumId());
-                }
-            });
+            final Song song = (Song) media;
+            return tryGetAlbumIcon(context, song.getAlbumId());
         }
 
         if (media.getKind() == Media.ALBUM) {
-            return Single.defer(new Callable<SingleSource<? extends BitmapResult>>() {
-                @Override
-                public SingleSource<? extends BitmapResult> call() throws Exception {
-                    final Album album = (Album) media;
-                    return getIconForAlbumId(context, album.getId());
-                }
-            });
+            final Album album = (Album) media;
+            return tryGetAlbumIcon(context, album.getId());
         }
 
-        return Single.just(new BitmapResult(null));
+        return null;
     }
 
-    private static Single<BitmapResult> getIconForAlbumId(@NonNull final Context context, long albumId) {
-        return Single.fromCallable(new Callable<BitmapResult>() {
-            @Override
-            public BitmapResult call() throws Exception {
-                final Uri artworkUri = Uri.parse("content://media/external/audio/albumart");
-                final Uri albumArtUri = ContentUris.withAppendedId(artworkUri, albumId);
+    @WorkerThread
+    @Nullable
+    private static Bitmap tryGetAlbumIcon(@NonNull final Context context, long albumId) {
+        final Uri artworkUri = Uri.parse("content://media/external/audio/albumart");
+        final Uri albumArtUri = ContentUris.withAppendedId(artworkUri, albumId);
 
-                try {
-                    final Bitmap original = MediaStore.Images.Media.getBitmap(
-                            context.getContentResolver(), albumArtUri);
-                    if (original == null) {
-                        return new BitmapResult(null);
-                    }
-
-                    // TODO: is 200x200 a good size for it?
-                    final Bitmap scaled =
-                            Bitmap.createScaledBitmap(original, 200, 200, true);
-
-                    if (original != scaled) {
-                        original.recycle();
-                    }
-
-                    return new BitmapResult(scaled);
-                } catch (Exception ignored) {
-                    return new BitmapResult(null);
-                }
+        try {
+            final Bitmap original = MediaStore.Images.Media.getBitmap(
+                    context.getContentResolver(), albumArtUri);
+            if (original == null) {
+                return null;
             }
-        });
+
+            // TODO: is 200x200 a good size for it?
+            final Bitmap scaled =
+                    Bitmap.createScaledBitmap(original, 200, 200, true);
+
+            if (original != scaled) {
+                original.recycle();
+            }
+
+            final float cornerRadiusInDp = 4f;
+            final float cornerRadiusInPx =
+                    TypedValue.applyDimension(
+                        TypedValue.COMPLEX_UNIT_DIP,
+                        cornerRadiusInDp,
+                        context.getResources().getDisplayMetrics()
+                    );
+            final Bitmap rounded = BitmapUtil.createRoundedBitmap(scaled, cornerRadiusInPx);
+
+            if (scaled != rounded) {
+                scaled.recycle();
+            }
+
+            return rounded;
+        } catch (Exception ignored) {
+            return null;
+        }
     }
 
     /**
@@ -189,37 +200,37 @@ final class Shortcuts {
      * @param media for which to create a shortcut intent
      * @return shortcut intent single source
      */
-    private static Single<Intent> createShortcutIntent(@NonNull final Context context, @NonNull final Media media) {
-        return Single.fromCallable(new Callable<Intent>() {
-            @Override
-            public Intent call() throws Exception {
-                if (media.getKind() == Media.SONG) {
-                    return MainActivity.Companion.newSongIntent(context, (Song) media);
-                }
+    @NonNull
+    private static Intent getShortcutIntent(
+        @NonNull final Context context,
+        @NonNull final Media media
+    ) throws UnknownMediaException {
 
-                if (media.getKind() == Media.ALBUM) {
-                    return MainActivity.Companion.newAlbumIntent(context, (Album) media);
-                }
+        if (media.getKind() == Media.SONG) {
+            return MainActivity.Companion.newSongIntent(context, (Song) media);
+        }
 
-                if (media.getKind() == Media.ARTIST) {
-                    return MainActivity.Companion.newArtistIntent(context, (Artist) media);
-                }
+        if (media.getKind() == Media.ALBUM) {
+            return MainActivity.Companion.newAlbumIntent(context, (Album) media);
+        }
 
-                if (media.getKind() == Media.GENRE) {
-                    return MainActivity.Companion.newGenreIntent(context, (Genre) media);
-                }
+        if (media.getKind() == Media.ARTIST) {
+            return MainActivity.Companion.newArtistIntent(context, (Artist) media);
+        }
 
-                if (media.getKind() == Media.PLAYLIST) {
-                    return MainActivity.Companion.newPlaylistIntent(context, (Playlist) media);
-                }
+        if (media.getKind() == Media.GENRE) {
+            return MainActivity.Companion.newGenreIntent(context, (Genre) media);
+        }
 
-                if (media.getKind() == Media.MY_FILE) {
-                    return MainActivity.Companion.newMyFileIntent(context, (MyFile) media);
-                }
+        if (media.getKind() == Media.PLAYLIST) {
+            return MainActivity.Companion.newPlaylistIntent(context, (Playlist) media);
+        }
 
-                throw new UnknownMediaException(media);
-            }
-        });
+        if (media.getKind() == Media.MY_FILE) {
+            return MainActivity.Companion.newMyFileIntent(context, (MyFile) media);
+        }
+
+        throw new UnknownMediaException(media);
     }
 
     static Single<Boolean> isShortcutSupported(@NonNull Context context, @NonNull Media media) {
@@ -237,13 +248,24 @@ final class Shortcuts {
     }
 
     static Completable createMediaShortcut(@NonNull final Context context, @NonNull final Media media) {
-        return Single.zip(createShortcutIntent(context, media), getIcon(context, media), new BiFunction<Intent, BitmapResult, Object>() {
+        return Single.fromCallable(new Callable<ShortcutParams>() {
             @Override
-            public Object apply(Intent intent, BitmapResult bitmapResult) throws Exception {
-                installShortcut_Internal(context, intent, media, bitmapResult.bitmap);
-                return new Object();
+            public ShortcutParams call() throws Exception {
+                final String shortcutId = getShortcutId(media);
+                final String shortcutLabel = getShortcutLabel(media);
+                final Intent shortcutIntent = getShortcutIntent(context, media);
+                final Bitmap icon = tryGetShortcutIcon(context, media);
+                return new ShortcutParams(shortcutId, shortcutLabel, shortcutIntent, icon);
             }
-        }).ignoreElement().subscribeOn(AndroidSchedulers.mainThread());
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSuccess(new Consumer<ShortcutParams>() {
+                    @Override
+                    public void accept(ShortcutParams shortcutParams) throws Exception {
+                        installShortcut_Internal(context, shortcutParams);
+                    }
+                })
+                .ignoreElement();
     }
 
     static Completable createSongShortcut(@NonNull Context context, @NonNull Song song) {
--- app/src/main/java/com/frolo/muse/util/BitmapUtil.java
+++ app/src/main/java/com/frolo/muse/util/BitmapUtil.java
@@ -3,21 +3,28 @@ package com.frolo.muse.util;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import androidx.annotation.NonNull;
+import androidx.annotation.WorkerThread;
+
+import com.frolo.muse.ThreadStrictMode;
 
 
 public final class BitmapUtil {
-    private BitmapUtil() { }
+    private BitmapUtil() {
+    }
 
-    // Worker thread
-    public static Bitmap drawableToBitmap(Drawable drawable, int overrideWidth, int overrideHeight) {
+    @WorkerThread
+    public static Bitmap getBitmap(Drawable drawable, int overrideWidth, int overrideHeight) {
+        ThreadStrictMode.assertBackground();
+
         if (drawable instanceof BitmapDrawable) {
             BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
             Bitmap bitmap = bitmapDrawable.getBitmap();
@@ -39,11 +46,13 @@ public final class BitmapUtil {
         return bitmap;
     }
 
-    // Worker thread
-    public static Bitmap drawableToBitmap(Drawable drawable) {
+    @WorkerThread
+    public static Bitmap getBitmap(Drawable drawable) {
+        ThreadStrictMode.assertBackground();
+
         final int width = drawable.getIntrinsicWidth();
         final int height = drawable.getIntrinsicHeight();
-        return drawableToBitmap(drawable, width, height);
+        return getBitmap(drawable, width, height);
     }
 
     public static Bitmap createTransparentBitmap(int width, int height, Bitmap.Config config) {
@@ -53,15 +62,30 @@ public final class BitmapUtil {
         return newBitmap;
     }
 
-    private static void saveBitmap(Bitmap bmp, File file, Bitmap.CompressFormat format) throws IOException {
-        final OutputStream fOut = new FileOutputStream(file);
-        bmp.compress(format, 100, fOut); // saving the Bitmap to a file compressed as a JPEG with 85% compression rate
-        fOut.flush(); // Not really required
-        fOut.close(); // do not forget to close the stream
-        //MediaStore.Images.Media.insertImage(contentResolver, file.getAbsolutePath(), file.getName(), file.getName());
+    @WorkerThread
+    public static Bitmap createRoundedBitmap(@NonNull Bitmap original, float cornerRadius) {
+        ThreadStrictMode.assertBackground();
+
+        final Bitmap output =
+                Bitmap.createBitmap(original.getWidth(), original.getHeight(), Bitmap.Config.ARGB_8888);
+        final Canvas canvas = new Canvas(output);
+
+
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, original.getWidth(), original.getHeight());
+        final RectF rectF = new RectF(rect);
+
+        paint.setAntiAlias(true);
+        paint.setColor(0xFFFFFFFF);
+        paint.setStyle(Paint.Style.FILL);
+
+        canvas.drawARGB(0, 0, 0, 0);
+        canvas.drawRoundRect(rectF, cornerRadius, cornerRadius, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(original, rect, rect, paint);
+
+        return output;
     }
 
-    public static void saveBitmapAsPng(Bitmap bmp, File file) throws IOException {
-        saveBitmap(bmp, file, Bitmap.CompressFormat.PNG);
-    }
 }
