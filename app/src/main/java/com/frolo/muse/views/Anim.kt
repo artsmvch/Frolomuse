6c658853903a81ad9b57cb690c89e4102bb761b9
Alexei <alexei.artsimovich@gmail.com>
2020-05-08 21:25:17 +0300

    refactored Anim object

--- app/src/main/java/com/frolo/muse/views/Anim.kt
+++ app/src/main/java/com/frolo/muse/views/Anim.kt
@@ -1,62 +1,57 @@
 package com.frolo.muse.views
 
-
 import android.animation.*
 import android.view.View
-import android.view.animation.Animation
+
 
 object Anim {
-    open class SimpleAnimationListener: Animation.AnimationListener {
-        override fun onAnimationStart(animation: Animation) = Unit
-        override fun onAnimationEnd(animation: Animation) = Unit
-        override fun onAnimationRepeat(animation: Animation) = Unit
-    }
 
-    fun paintBackground(fromColor: Int, toColor: Int, duration: Long = 200, delay: Long = 0): ValueAnimator {
-        return ValueAnimator().also { anim ->
-            anim.setIntValues(fromColor, toColor)
-            anim.setEvaluator(ArgbEvaluator())
-            anim.duration = duration
-            anim.startDelay = delay
-        }
+    private open class SimpleAnimatorListener: Animator.AnimatorListener {
+        override fun onAnimationRepeat(animation: Animator?) = Unit
+
+        override fun onAnimationEnd(animation: Animator?) = Unit
+
+        override fun onAnimationCancel(animation: Animator?) = Unit
+
+        override fun onAnimationStart(animation: Animator?) = Unit
     }
 
     fun fadeIn(view: View, duration: Long = 200, delay: Long = 0) {
         view.animate()
-                .alpha(1f)
-                .setListener(object : Animator.AnimatorListener {
-                    override fun onAnimationRepeat(animation: Animator?) = Unit
-                    override fun onAnimationEnd(animation: Animator?) {
-                        view.visibility = View.VISIBLE
-                    }
-                    override fun onAnimationCancel(animation: Animator?) = Unit
-                    override fun onAnimationStart(animation: Animator?) {
-                        view.visibility = View.VISIBLE
-                    }})
-                .setDuration(duration)
-                .setStartDelay(delay)
-                .start()
+            .alpha(1f)
+            .setListener(object : SimpleAnimatorListener() {
+                override fun onAnimationEnd(animation: Animator?) {
+                    view.visibility = View.VISIBLE
+                }
+
+                override fun onAnimationStart(animation: Animator?) {
+                    view.visibility = View.VISIBLE
+                }
+            })
+            .setDuration(duration)
+            .setStartDelay(delay)
+            .start()
     }
 
     fun fadeOut(view: View, duration: Long = 200, delay: Long = 0) {
         view.animate()
-                .alpha(0f)
-                .setListener(object : Animator.AnimatorListener {
-                    override fun onAnimationRepeat(animation: Animator?) = Unit
-                    override fun onAnimationEnd(animation: Animator?) {
-                        view.visibility = View.INVISIBLE
-                    }
-                    override fun onAnimationCancel(animation: Animator?) = Unit
-                    override fun onAnimationStart(animation: Animator?) {
-                        view.visibility = View.VISIBLE
-                    }})
-                .setDuration(duration)
-                .setStartDelay(delay)
-                .start()
-    }
+            .alpha(0f)
+            .setListener(object : SimpleAnimatorListener() {
+                override fun onAnimationEnd(animation: Animator?) {
+                    view.visibility = View.INVISIBLE
+                }
 
-    fun alpha(view: View, toAlpha: Float, duration: Long = 200, delay: Long = 0) {
-        view.animate().alpha(toAlpha).setDuration(duration).setStartDelay(delay).start()
+                override fun onAnimationStart(animation: Animator?) {
+                    // Do NOT set the view's visibility to VISIBLE when the animation starts.
+                    // Otherwise, if the view's visibility was invisible(gone) then for the user,
+                    // the view appears for the specified duration.
+                    // But in fact, the view should not appear only to show the animation.
+                    // If the view is invisible(gone) to the user then it should remain invisible(gone) to the user.
+                }
+            })
+            .setDuration(duration)
+            .setStartDelay(delay)
+            .start()
     }
 
     fun like(like: View, duration: Long = 200) {
@@ -70,4 +65,5 @@ object Anim {
     fun unlike(like: View, duration: Long = 200) {
         like(like, duration)
     }
+
 }
