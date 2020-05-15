589928dbe0f222d364defe240c1b4759a5368ee1
Alexei <alexei.artsimovich@gmail.com>
2020-05-15 15:50:08 +0300

    refactored PulseTouchDownListener

--- app/src/main/java/com/frolo/muse/ui/main/player/PulseTouchDownListener.kt
+++ app/src/main/java/com/frolo/muse/ui/main/player/PulseTouchDownListener.kt
@@ -11,50 +11,62 @@ class PulseTouchDownListener constructor(
     private val period: Long,
     private val onPulse: () -> Unit
 ): View.OnTouchListener {
-    private var holdingTouchDown = false
+
+    // The current timer.
     private var timer: Timer? = null
+
+    // Indicates whether the current timer has been pulsed at least once.
+    // This flag is set to false when the timer is scheduled/cancelled.
+    private var pulsed: Boolean = false
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouch(v: View, event: MotionEvent): Boolean {
         return when(event.action) {
             MotionEvent.ACTION_DOWN -> {
+
+                // Cancel the previous timer, if any
                 timer?.apply {
                     cancel()
                     purge()
                 }
-                timer = Timer().also { timer ->
+
+                pulsed = false
+                timer = Timer("pulse_touch_down").also { timer ->
                     timer.schedule(object : TimerTask() {
                         override fun run() {
-                            holdingTouchDown = true
                             v.post {
                                 if (v.isAttachedToWindow) {
-                                    onPulse()
+                                    pulsed = true
+                                    onPulse.invoke()
                                 } else {
-                                    holdingTouchDown = false
                                     cancel()
                                 }
                             }
                         }
                     }, delay, period)
                 }
+
                 false
             }
 
             MotionEvent.ACTION_UP, MotionEvent.ACTION_OUTSIDE, MotionEvent.ACTION_CANCEL -> {
-                if (!holdingTouchDown) {
-                    timer?.apply {
-                        cancel()
-                        purge()
-                    }
-                    false
-                } else {
-                    holdingTouchDown = false
-                    timer?.apply {
-                        cancel()
-                        purge()
-                    }
-                    true
+                // The result is the value of the 'pulsed' flag.
+                // If the timer has not been pulsed at all, then the result is false and the touch is handled by default.
+                // Otherwise, the result is true.
+                val result = pulsed
+
+                timer?.apply {
+                    cancel()
+                    purge()
                 }
+                pulsed = false
+
+                if (result) {
+                    // Clear the pressed state
+                    v.isPressed = false
+                }
+
+                result
             }
 
             else -> false
