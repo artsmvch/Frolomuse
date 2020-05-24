b6b67274852853fe058893ed7d4d0905ebf034c4
Alexei <alexei.artsimovich@gmail.com>
2020-05-24 13:40:28 +0300

    updated AndroidEventLogger class

--- app/src/main/java/com/frolo/muse/logger/AndroidEventLogger.java
+++ app/src/main/java/com/frolo/muse/logger/AndroidEventLogger.java
@@ -2,11 +2,37 @@ package com.frolo.muse.logger;
 
 import android.util.Log;
 
+import androidx.annotation.Nullable;
+
 import java.util.Map;
 
+
 class AndroidEventLogger implements EventLogger {
+
     private String buildTag() {
-        return "Frolomuse_";
+        return "FrolomuseEvent";
+    }
+
+    @Nullable
+    private String getParamsString(@Nullable final Map<String, String> params) {
+        if (params == null || params.size() == 0)
+            return null;
+
+        final StringBuilder sb = new StringBuilder();
+
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            sb.append(entry.getKey());
+            sb.append('=');
+            sb.append(entry.getValue());
+            sb.append('&');
+        }
+
+        if (sb.length() >= 1) {
+            // Removing the last '&'
+            sb.deleteCharAt(sb.length() - 1);
+        }
+
+        return sb.toString();
     }
 
     @Override
@@ -16,12 +42,17 @@ class AndroidEventLogger implements EventLogger {
 
     @Override
     public void log(String event, Map<String, String> params) {
-        // TODO: params are not used, need to fix that
-        Log.d(buildTag(), event);
+        final String paramsString = getParamsString(params);
+        if (paramsString != null && !paramsString.isEmpty()) {
+            Log.d(buildTag(), event + " [" + paramsString + "]");
+        } else {
+            Log.d(buildTag(), event);
+        }
     }
 
     @Override
     public void log(Throwable err) {
-        Log.e(buildTag(), "", err);
+        Log.e(buildTag(), "Error", err);
     }
+
 }
