2d3037b18cba8cc7d4dbea1d0f7b90a65db3d3c5
Alexei <alexei.artsimovich@gmail.com>
2020-08-31 22:59:02 +0300

    updated DeleteMediaUseCase

--- app/src/main/java/com/frolo/muse/interactor/media/DeleteMediaUseCase.kt
+++ app/src/main/java/com/frolo/muse/interactor/media/DeleteMediaUseCase.kt
@@ -4,55 +4,41 @@ import com.frolo.muse.common.toAudioSources
 import com.frolo.muse.engine.Player
 import com.frolo.muse.model.media.Media
 import com.frolo.muse.model.media.Playlist
-import com.frolo.muse.model.media.Song
 import com.frolo.muse.repository.MediaRepository
 import com.frolo.muse.rx.SchedulerProvider
 import io.reactivex.Completable
 import io.reactivex.Single
-import io.reactivex.functions.Function
 
 
 class DeleteMediaUseCase <E: Media> constructor(
-        private val schedulerProvider: SchedulerProvider,
-        private val repository: MediaRepository<E>,
-        private val player: Player
+    private val schedulerProvider: SchedulerProvider,
+    private val repository: MediaRepository<E>,
+    private val player: Player
 ) {
 
     fun delete(item: E): Completable {
-        val operator = if (item is Playlist) {
+        val completable = if (item is Playlist) {
+            // If the item is a Playlist then we simply delete it.
             repository.delete(item)
         } else {
+            // Otherwise, we collect all related to this item songs and remove those songs from the player.
+            // After that, we delete the item.
             repository.collectSongs(item)
-                    .flatMapCompletable { songsRelatedToItem ->
-                        repository.delete(item)
-                                .doOnComplete {
-                                    val audioSources = songsRelatedToItem.toAudioSources()
-                                    player.removeAll(audioSources)
-                                }
-                    }
+                .flatMapCompletable { songsRelatedToItem ->
+                    repository.delete(item)
+                        .doOnComplete {
+                            val audioSources = songsRelatedToItem.toAudioSources()
+                            player.removeAll(audioSources)
+                        }
+                }
         }
-        return operator
-                .subscribeOn(schedulerProvider.worker())
+        return completable.subscribeOn(schedulerProvider.worker())
     }
 
     fun delete(items: Collection<E>): Completable {
-        val removeFromPlaylist = items.let { list ->
-            val sources = list.map { item ->
-                repository.collectSongs(item)
-            }
-
-            Single.zip(sources, Function<Array<Any>, List<Song>> { arr ->
-                arr.flatMap {
-                    @Suppress("UNCHECKED_CAST")
-                    it as Collection<Song>
-                }
-            })
-        }
-
-        return removeFromPlaylist
-                .doOnSuccess { songs -> player.removeAll(songs.toAudioSources()) }
-                .flatMapCompletable { repository.delete(items) }
-                .subscribeOn(schedulerProvider.worker())
+        return Single.fromCallable { items.map { delete(it) } }
+            .subscribeOn(schedulerProvider.computation())
+            .flatMapCompletable { Completable.concat(it) }
     }
 
 }
