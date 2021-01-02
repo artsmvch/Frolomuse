1af31985f8f071ea0e0d97a3fa3ac28f59dfcf6a
Alexei <alexei.artsimovich@gmail.com>
2021-01-02 18:43:33 +0300

    added open count threshold for ads

--- app/src/main/java/com/frolo/muse/interactor/ads/ShowBannerUseCase.kt
+++ app/src/main/java/com/frolo/muse/interactor/ads/ShowBannerUseCase.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import com.frolo.muse.BuildConfig
 import com.frolo.muse.admob.AdMobs
 import com.frolo.muse.android.firstPackageInstallTime
+import com.frolo.muse.repository.Preferences
 import io.reactivex.Single
 import javax.inject.Inject
 
 class ShowBannerUseCase @Inject constructor(
-    private val context: Context
+    private val context: Context,
+    private val preferences: Preferences
 ) {
 
     fun canShowBanner(): Single<Boolean> {
@@ -25,6 +27,10 @@ class ShowBannerUseCase @Inject constructor(
             return Single.just(false)
         }
 
+        if (preferences.openCount < OPEN_COUNT_THRESHOLD_FOR_AD) {
+            return Single.just(false)
+        }
+
         // immediately fetch the configs from the server
         return AdMobs.isAdMobEnabled(minimumFetchIntervalInSeconds = 0)
     }
@@ -33,6 +39,9 @@ class ShowBannerUseCase @Inject constructor(
         private val DEBUG = BuildConfig.DEBUG
 
         private const val INSTALL_TIME_THRESHOLD_FOR_AD = 1609459200L // 2020.01.01 00:00:00
+
+        // The min open count to show ads to the user
+        private const val OPEN_COUNT_THRESHOLD_FOR_AD = 10
 
         private fun now(): Long = System.currentTimeMillis()
     }
