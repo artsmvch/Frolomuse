c4d05b0a6d92247bd9d611b11489c2b88f314f4f
a.artimovich <a.artimovich@corp.vk.com>
2021-05-05 12:04:20 +0300

    fixed setting data source in PlayerImpl

--- app/src/main/java/com/frolo/muse/engine/PlayerImpl.java
+++ app/src/main/java/com/frolo/muse/engine/PlayerImpl.java
@@ -637,14 +637,20 @@ public final class PlayerImpl implements Player {
 
                     try {
                         engine.reset();
-                        if (item instanceof MediaStoreRow) {
-                            Uri uri = ((MediaStoreRow) item).getUri();
-                            mPlayerJournal.logMessage("Set data source from uri: " + uri.toString());
-                            engine.setDataSource(mContext, uri);
-                        } else {
-                            String path = item.getSource();
-                            mPlayerJournal.logMessage("Set data source from path: " + path);
-                            engine.setDataSource(path);
+                        try {
+                            // First, try to set the data source by the filepath
+                            String filepath = item.getSource();
+                            mPlayerJournal.logMessage("Set data source from path: " + filepath);
+                            engine.setDataSource(filepath);
+                        } catch (Throwable error) {
+                            // If failed, try to set the data source by the uri
+                            if (item instanceof MediaStoreRow) {
+                                Uri uri = ((MediaStoreRow) item).getUri();
+                                mPlayerJournal.logMessage("Set data source from uri: " + uri.toString());
+                                engine.setDataSource(mContext, uri);
+                            } else {
+                                throw error;
+                            }
                         }
                         engine.prepare();
 
