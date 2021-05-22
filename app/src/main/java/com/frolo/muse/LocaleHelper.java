b97e39da93e1f8c60356e663ec2b2db34894e9e5
Alexei <alexei.artsimovich@gmail.com>
2021-05-22 15:20:00 +0300

    updated methods for applying language to context

--- app/src/main/java/com/frolo/muse/LocaleHelper.java
+++ app/src/main/java/com/frolo/muse/LocaleHelper.java
@@ -2,8 +2,11 @@ package com.frolo.muse;
 
 import android.content.Context;
 import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 
 import java.util.Locale;
 
@@ -12,6 +15,16 @@ public final class LocaleHelper {
 
     @NonNull
     public static Context applyLanguage(@NonNull Context context, @NonNull String lang) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return applyLanguageApi24Impl(context, lang);
+        } else {
+            return applyLanguageImpl(context, lang);
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    @NonNull
+    private static Context applyLanguageApi24Impl(@NonNull Context context, @NonNull String lang) {
         Locale locale = new Locale(lang);
         Locale.setDefault(locale);
 
@@ -22,6 +35,24 @@ public final class LocaleHelper {
         Configuration newConfiguration = new Configuration(configuration);
 
         return context.createConfigurationContext(newConfiguration);
+    }
+
+    // TODO: not working on Android API 23
+    // Legacy approach
+    @NonNull
+    private static Context applyLanguageImpl(@NonNull Context context, @NonNull String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+
+        Resources resources = context.getResources();
+
+        Configuration configuration = resources.getConfiguration();
+        configuration.locale = locale;
+        configuration.setLayoutDirection(locale);
+
+        resources.updateConfiguration(configuration, resources.getDisplayMetrics());
+
+        return context;
     }
 
     private LocaleHelper() {
