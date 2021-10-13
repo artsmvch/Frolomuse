221ea5d9792a53269b9c5bb3d2a1efaba0150fbc
a.artimovich <a.artimovich@corp.vk.com>
2021-10-13 21:04:27 +0300

    updated content executors

--- app/src/main/java/com/frolo/muse/di/impl/local/ContentExecutors.java
+++ app/src/main/java/com/frolo/muse/di/impl/local/ContentExecutors.java
@@ -2,8 +2,12 @@ package com.frolo.muse.di.impl.local;
 
 import androidx.annotation.NonNull;
 
+import com.frolo.muse.DebugUtils;
+
 import java.util.concurrent.Executor;
-import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -22,23 +26,49 @@ import io.reactivex.schedulers.Schedulers;
         static final int MAX_THREAD_NUMBER = 32;
         static final long KEEP_ALIVE_TIME_IN_SECONDS = 10L;
 
+        private static final AtomicLong sThreadId = new AtomicLong(0);
+        static final ThreadFactory sThreadFactory = r -> {
+            String name = "ContentWorker-" + sThreadId.getAndIncrement();
+            return new Thread(r, name);
+        };
+
+        static final RejectedExecutionHandler sRejectedExecutionHandler = (r, executor) -> {
+            RejectedExecutionException exception = new RejectedExecutionException(
+                    "Task " + r.toString() + " rejected from " + executor.toString());
+            DebugUtils.dumpOnMainThread(exception);
+            throw exception;
+        };
+
         static final Executor sInstance;
 
         static {
-            ThreadFactory threadFactory = new ThreadFactory() {
-                final AtomicLong threadId = new AtomicLong(0);
-
-                @Override
-                public Thread newThread(Runnable r) {
-                    String name = "ContentWorker-" + threadId.getAndIncrement();
-                    return new Thread(r, name);
-                }
-            };
-            sInstance = new ThreadPoolExecutor(CORE_THREAD_NUMBER, MAX_THREAD_NUMBER,
-                    KEEP_ALIVE_TIME_IN_SECONDS, TimeUnit.SECONDS,
-                    new SynchronousQueue<Runnable>(),
-                    threadFactory);
+            sInstance = createInstance0();
         }
+
+        private static Executor createInstance0() {
+            // allow core thread timeout to timeout the core threads so that
+            // when no tasks arrive, the core threads can be killed
+            ThreadPoolExecutor executor = new ThreadPoolExecutor(CORE_THREAD_NUMBER, MAX_THREAD_NUMBER,
+                    KEEP_ALIVE_TIME_IN_SECONDS, TimeUnit.SECONDS,
+                    new LinkedBlockingDeque<Runnable>() /* unbounded queue*/,
+                    sThreadFactory, sRejectedExecutionHandler);
+            executor.allowCoreThreadTimeOut(true);
+            return executor;
+        }
+
+//        private static Executor createInstance0() {
+//            ExecutorService executor = Executors.newFixedThreadPool(MAX_THREAD_NUMBER, sThreadFactory);
+//            if (executor instanceof ThreadPoolExecutor) {
+//                // Be careful with this shit... It may crash
+//                ThreadPoolExecutor pool = (ThreadPoolExecutor) executor;
+//                pool.setCorePoolSize(CORE_THREAD_NUMBER);
+//                pool.setKeepAliveTime(KEEP_ALIVE_TIME_IN_SECONDS, TimeUnit.SECONDS);
+//                pool.allowCoreThreadTimeOut(true);
+//                pool.setRejectedExecutionHandler(sRejectedExecutionHandler);
+//            }
+//            return executor;
+//        }
+
     }
 
     private static class WorkerSchedulerHolder {
