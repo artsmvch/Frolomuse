cb6bcf83b209b684b646b571ed97bbf5f8646477
a.artimovich <a.artimovich@corp.vk.com>
2021-10-13 17:18:13 +0300

    updated configuration for worker executor

--- app/src/main/java/com/frolo/muse/di/impl/local/ExecutorHolder.java
+++ app/src/main/java/com/frolo/muse/di/impl/local/ExecutorHolder.java
@@ -1,31 +1,41 @@
 package com.frolo.muse.di.impl.local;
 
 import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
 import io.reactivex.Scheduler;
 import io.reactivex.schedulers.Schedulers;
 
 
-final class ExecutorHolder {
+/* package-private */ final class ExecutorHolder {
 
     private static class WorkerExecutor {
+
+        // Configuration
+        static final int CORE_THREAD_NUMBER = 8;
+        static final int MAX_THREAD_NUMBER = 32;
+        static final long KEEP_ALIVE_TIME_IN_SECONDS = 10L;
+
         static final Executor sInstance;
 
         static {
-            ThreadFactory factory = new ThreadFactory() {
-
+            ThreadFactory threadFactory = new ThreadFactory() {
                 final AtomicLong threadId = new AtomicLong(0);
 
                 @Override
                 public Thread newThread(Runnable r) {
-                    String name = "ContentQueryExecutor-" + threadId.getAndIncrement();
+                    String name = "ContentWorker-" + threadId.getAndIncrement();
                     return new Thread(r, name);
                 }
             };
-            sInstance = Executors.newCachedThreadPool(factory);
+            sInstance = new ThreadPoolExecutor(CORE_THREAD_NUMBER, MAX_THREAD_NUMBER,
+                    KEEP_ALIVE_TIME_IN_SECONDS, TimeUnit.SECONDS,
+                    new LinkedBlockingQueue<Runnable>(),
+                    threadFactory);
         }
     }
 
