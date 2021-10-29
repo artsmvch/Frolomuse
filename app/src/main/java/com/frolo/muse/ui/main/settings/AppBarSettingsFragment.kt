2d22a4a9f9fcb1f1f384cbbe560bee31eae6ecfb
Alexei <alexei.artsimovich@gmail.com>
2021-10-29 23:10:22 +0300

    fixed scrolled-to-top feature for appbar settings screen

--- app/src/main/java/com/frolo/muse/ui/main/settings/AppBarSettingsFragment.kt
+++ app/src/main/java/com/frolo/muse/ui/main/settings/AppBarSettingsFragment.kt
@@ -6,13 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
 import com.frolo.muse.R
+import com.frolo.muse.ui.ScrolledToTop
 import com.frolo.muse.ui.base.BaseFragment
 import kotlinx.android.synthetic.main.fragment_appbar_settings.*
 
 
 // Simple wrapper for the SettingsFragment, this only puts a Toolbar at the top of it.
-class AppBarSettingsFragment : BaseFragment() {
+class AppBarSettingsFragment : BaseFragment(), ScrolledToTop {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,6 +26,18 @@ class AppBarSettingsFragment : BaseFragment() {
         (activity as? AppCompatActivity)?.apply {
             setSupportActionBar(tb_actions as Toolbar)
             supportActionBar?.setTitle(R.string.nav_settings)
+        }
+    }
+
+    private fun peekInnerFragment(): Fragment? {
+        return childFragmentManager.findFragmentByTag("settings")
+    }
+
+    override fun scrollToTop() {
+        app_bar_layout?.setExpanded(true, true)
+        val innerFragment = peekInnerFragment()
+        if (innerFragment != null && innerFragment is ScrolledToTop) {
+            innerFragment.scrollToTop()
         }
     }
 
