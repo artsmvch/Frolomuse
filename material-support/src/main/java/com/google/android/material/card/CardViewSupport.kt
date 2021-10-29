264a9eedd0311f695411de5e2033d53380f2f987
Alexei <alexei.artsimovich@gmail.com>
2021-10-29 15:04:49 +0300

    added compat method for changing card view shadow color

--- material-support/src/main/java/com/google/android/material/card/CardViewSupport.kt
+++ material-support/src/main/java/com/google/android/material/card/CardViewSupport.kt
@@ -1,5 +1,7 @@
 package com.google.android.material.card
 
+import android.annotation.SuppressLint
+import androidx.annotation.ColorInt
 import kotlin.math.cos
 
 
@@ -17,6 +19,18 @@ object CardViewSupport {
      */
     fun calculateCardVerticalShadowPadding(maxCardElevation: Float, cornerRadius: Float): Float {
         return (maxCardElevation * 1.5 + (1 - cos(Math.PI / 4)) * cornerRadius).toFloat()
+    }
+
+    @SuppressLint("RestrictedApi")
+    fun setShadowColor(cardView: MaterialCardView, @ColorInt color: Int) {
+        try {
+            val cardViewHelperField = MaterialCardView::class.java.getDeclaredField("cardViewHelper")
+            cardViewHelperField.isAccessible = true
+            val cardViewHelper = cardViewHelperField.get(cardView) as MaterialCardViewHelper
+            val background = cardViewHelper.background
+            background.setShadowColor(color)
+        } catch (ignored: Throwable) {
+        }
     }
 
 }
