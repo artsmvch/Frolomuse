e39fea1cdf9949bcfbdccc048b00c403234263c0
Alexei <alexei.artsimovich@gmail.com>
2022-03-13 23:15:15 +0300

    added docs to SchedulerProvider

--- app/src/main/java/com/frolo/muse/rx/SchedulerProvider.kt
+++ app/src/main/java/com/frolo/muse/rx/SchedulerProvider.kt
@@ -2,10 +2,20 @@ package com.frolo.muse.rx
 
 import io.reactivex.Scheduler
 
+
 interface SchedulerProvider {
+    /**
+     * Scheduler for long running blocking operations, typically for I/O-bound work.
+     */
     fun worker(): Scheduler
 
+    /**
+     * Scheduler for computational work.
+     */
     fun computation(): Scheduler
 
+    /**
+     * Main thread scheduler.
+     */
     fun main(): Scheduler
 }
