c08372ae3e580a9fba274bc6beb7871e1ff54e1a
Alexei <alexei.artsimovich@gmail.com>
2022-05-08 23:36:06 +0300

    refactored

--- app/src/main/java/com/frolo/muse/ui/base/FragmentContentInsetsListener.kt
+++ app/src/main/java/com/frolo/muse/ui/base/FragmentContentInsetsListener.kt
@@ -4,7 +4,7 @@ package com.frolo.muse.ui.base
 /**
  * Listener for applying content insets to a fragment.
  */
-interface FragmentContentInsetsListener {
+fun interface FragmentContentInsetsListener {
 
     fun applyContentInsets(left: Int, top: Int, right: Int, bottom: Int)
 
