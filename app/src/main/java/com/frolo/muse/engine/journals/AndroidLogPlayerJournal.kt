72f03e54b5ad56c6f420ca75a38efcca618e83ac
Alexei <alexei.artsimovich@gmail.com>
2022-05-23 23:16:45 +0300

    removed unnecessary synchronization from AndroidLogPlayerJournal

--- app/src/main/java/com/frolo/muse/engine/journals/AndroidLogPlayerJournal.kt
+++ app/src/main/java/com/frolo/muse/engine/journals/AndroidLogPlayerJournal.kt
@@ -6,22 +6,15 @@ import com.frolo.player.PlayerJournal
 
 class AndroidLogPlayerJournal(val tag: String): PlayerJournal {
 
-    private val logLock = Any()
-
-    @Synchronized
     override fun logMessage(message: String?) {
-        synchronized(logLock) {
-            Log.d(tag, message.orEmpty())
-        }
+        Log.d(tag, message.orEmpty())
     }
 
     override fun logError(message: String?, error: Throwable?) {
-        synchronized(logLock) {
-            if (error != null) {
-                Log.e(tag, message.orEmpty(), error)
-            } else {
-                Log.d(tag, message.orEmpty())
-            }
+        if (error != null) {
+            Log.e(tag, message.orEmpty(), error)
+        } else {
+            Log.d(tag, message.orEmpty())
         }
     }
 
