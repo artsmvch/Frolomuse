48743bad221893151a6bcd522f32c5ec7f7638f6
a.artimovich <a.artimovich@corp.vk.com>
2022-07-06 12:33:21 +0300

    minor refactoring

--- app/src/main/java/com/frolo/muse/ui/ScrolledToTop.kt
+++ app/src/main/java/com/frolo/muse/ui/ScrolledToTop.kt
@@ -4,6 +4,6 @@ package com.frolo.muse.ui
 /**
  * Indicates that the UI component is able to scroll to the top of its content.
  */
-interface ScrolledToTop {
+fun interface ScrolledToTop {
     fun scrollToTop()
 }
