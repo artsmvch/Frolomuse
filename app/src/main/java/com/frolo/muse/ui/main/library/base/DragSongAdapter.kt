e1109070d4cb4e353ced71ea6d77b1628c255874
Alexei <alexei.artsimovich@gmail.com>
2022-07-18 21:21:22 +0300

    updated drag burger icon

--- app/src/main/java/com/frolo/muse/ui/main/library/base/DragSongAdapter.kt
+++ app/src/main/java/com/frolo/muse/ui/main/library/base/DragSongAdapter.kt
@@ -7,22 +7,22 @@ import android.view.ViewGroup
 import androidx.core.view.updateLayoutParams
 import androidx.recyclerview.widget.RecyclerView
 import com.frolo.muse.R
-import com.frolo.ui.StyleUtils
-import com.frolo.muse.dp2px
 import com.frolo.muse.inflateChild
-import com.frolo.music.model.Song
 import com.frolo.muse.thumbnails.ThumbnailLoader
 import com.frolo.muse.ui.base.adapter.ItemTouchHelperAdapter
 import com.frolo.muse.ui.getArtistString
 import com.frolo.muse.ui.getDurationString
 import com.frolo.muse.ui.getNameString
 import com.frolo.muse.views.media.MediaConstraintLayout
+import com.frolo.music.model.Song
+import com.frolo.ui.Screen
+import com.frolo.ui.StyleUtils
 import kotlinx.android.synthetic.main.include_check.view.*
 import kotlinx.android.synthetic.main.include_song_art_container.view.*
 import kotlinx.android.synthetic.main.item_drag_song.view.*
 
 
-class DragSongAdapter constructor(
+open class DragSongAdapter constructor(
     private val thumbnailLoader: ThumbnailLoader,
     private val onDragListener: OnDragListener? = null
 ): SongAdapter<Song>(thumbnailLoader), ItemTouchHelperAdapter {
@@ -126,7 +126,7 @@ class DragSongAdapter constructor(
 
     class DragSongViewHolder(itemView: View): SongViewHolder(itemView) {
 
-        private val dragIconSize: Int = 16f.dp2px(itemView.context).toInt()
+        private val dragIconSize: Int = Screen.dp(itemView.context, 24)
 
         init {
             itemView.imv_song_thumbnail.apply {
@@ -134,7 +134,7 @@ class DragSongAdapter constructor(
                     width = dragIconSize
                     height = dragIconSize
                 }
-                setImageResource(R.drawable.ic_drag_burger)
+                setImageResource(R.drawable.ic_drag_burger_outline_28)
                 imageTintList =
                     ColorStateList.valueOf(StyleUtils.resolveColor(context, R.attr.iconImageTint))
             }
