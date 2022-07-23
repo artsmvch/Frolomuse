431b37f4c1d2e1115f83040789bb0fcd04c5f240
Alexei <alexei.artsimovich@gmail.com>
2022-07-23 18:51:34 +0300

    optimized skipped window insets listener

--- app/src/main/java/com/frolo/muse/ui/main/WindowInsetsHelper.kt
+++ app/src/main/java/com/frolo/muse/ui/main/WindowInsetsHelper.kt
@@ -4,12 +4,16 @@ import android.view.View
 
 
 internal object WindowInsetsHelper {
+    private val SKIPPED_WINDOW_INSETS_LISTENER = View.OnApplyWindowInsetsListener { _, insets ->
+        insets
+    }
+
     /**
      * Causes [view] to skip processing and changing window insets.
      * Window insets will just be dispatched down the hierarchy.
      */
     fun skipWindowInsets(view: View) {
-        setupWindowInsets(view) { _, insets -> insets }
+        setupWindowInsets(view, SKIPPED_WINDOW_INSETS_LISTENER)
     }
 
     fun setupWindowInsets(view: View, listener: View.OnApplyWindowInsetsListener) {
