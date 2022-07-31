2885e245c822d250c87eec84b70bd8bd3d9be18b
a.artimovich <a.artimovich@corp.vk.com>
2022-07-31 10:47:09 +0300

    updated AudioFx error handler

--- app/src/main/java/com/frolo/muse/player/service/audiofx/DefaultAudioFxErrorHandler.kt
+++ app/src/main/java/com/frolo/muse/player/service/audiofx/DefaultAudioFxErrorHandler.kt
@@ -1,15 +1,21 @@
 package com.frolo.muse.player.service.audiofx
 
+import android.widget.Toast
 import com.frolo.audiofx.AudioFxImpl
+import com.frolo.muse.ActivityWatcher
+import com.frolo.muse.BuildInfo
 import com.frolo.muse.rx.newSingleThreadScheduler
 import com.frolo.muse.rx.subscribeSafely
+import com.frolo.ui.ActivityUtils
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import io.reactivex.Completable
 import io.reactivex.Scheduler
 import io.reactivex.disposables.CompositeDisposable
 
 
-class DefaultAudioFxErrorHandler : AudioFxImpl.ErrorHandler {
+class DefaultAudioFxErrorHandler(
+    private val activityWatcher: ActivityWatcher
+) : AudioFxImpl.ErrorHandler {
 
     private val disposables = CompositeDisposable()
 
@@ -19,10 +25,27 @@ class DefaultAudioFxErrorHandler : AudioFxImpl.ErrorHandler {
 
     override fun onError(error: Throwable?) {
         error ?: return
+        dispatchErrorToFirebaseAsync(error)
+        toastError(error)
+    }
+
+    private fun dispatchErrorToFirebaseAsync(error: Throwable) {
         Completable.fromAction { FirebaseCrashlytics.getInstance().recordException(error) }
             .subscribeOn(workerScheduler)
             .subscribeSafely()
             .also { disposables.add(it) }
+    }
+
+    private fun toastError(error: Throwable) {
+        if (!BuildInfo.isDebug()) {
+            return
+        }
+        activityWatcher.foregroundActivity?.runOnUiThread {
+            val activity = activityWatcher.foregroundActivity
+            if (activity != null && !ActivityUtils.isFinishingOrDestroyed(activity)) {
+                Toast.makeText(activity, "AudioFx error: $error", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
 }
