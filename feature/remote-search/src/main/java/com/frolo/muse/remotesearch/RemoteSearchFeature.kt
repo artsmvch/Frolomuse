693ae7e685aae26b2120bb1253ea3be7de2d29e1
Alexei <alexei.artsimovich@gmail.com>
2022-08-04 13:22:09 +0300

    added remotesearch feature module

---
+++ feature/remote-search/src/main/java/com/frolo/muse/remotesearch/RemoteSearchFeature.kt
@@ -0,0 +1,9 @@
+package com.frolo.muse.remotesearch
+
+import androidx.fragment.app.Fragment
+
+object RemoteSearchFeature {
+    fun newScreen(): Fragment {
+        throw UnsupportedOperationException("Not implemented yet")
+    }
+}
