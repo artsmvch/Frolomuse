34a58b6dc3db6ccf60de532b49abaca9b0c2bdb7
Alexei <alexei.artsimovich@gmail.com>
2022-08-09 19:51:50 +0300

    fixed synchronization issues in ApplicationForegroundStatusRegistryImpl

--- core-ui/src/main/java/com/frolo/core/ui/application/ApplicationForegroundStatusRegistryImpl.kt
+++ core-ui/src/main/java/com/frolo/core/ui/application/ApplicationForegroundStatusRegistryImpl.kt
@@ -2,44 +2,55 @@ package com.frolo.core.ui.application
 
 import android.app.Activity
 import android.app.Application
+import androidx.annotation.GuardedBy
 import com.frolo.ui.SimpleActivityLifecycleCallbacks
+import java.util.concurrent.atomic.AtomicInteger
 
 internal class ApplicationForegroundStatusRegistryImpl:
     ApplicationForegroundStatusRegistry,
     Application.ActivityLifecycleCallbacks by SimpleActivityLifecycleCallbacks() {
 
-    private var startedActivityCount: Int = 0
+    private val startedActivityCounter = AtomicInteger(0)
+
+    private val observersLock = Any()
+    @GuardedBy("observersLock")
     private val observers = HashSet<ApplicationForegroundStatusRegistry.Observer>()
 
-    override val isInForeground: Boolean get() = startedActivityCount > 0
+    override val isInForeground: Boolean get() = startedActivityCounter.get() > 0
 
     override val isInBackground: Boolean get() = !isInForeground
 
     override fun addObserver(observer: ApplicationForegroundStatusRegistry.Observer) {
-        observers.add(observer)
+        synchronized(observersLock) {
+            observers.add(observer)
+        }
     }
 
     override fun removeObserver(observer: ApplicationForegroundStatusRegistry.Observer) {
-        observers.remove(observer)
+        synchronized(observersLock) {
+            observers.remove(observer)
+        }
     }
 
     override fun onActivityStarted(activity: Activity) {
-        startedActivityCount++
-        if (startedActivityCount == 1) {
+        val newCount = startedActivityCounter.incrementAndGet()
+        if (newCount == 1) {
             dispatchApplicationForegroundStatus(isInForeground = true)
         }
     }
 
     override fun onActivityStopped(activity: Activity) {
-        startedActivityCount--
-        if (startedActivityCount == 0) {
+        val newCount = startedActivityCounter.decrementAndGet()
+        if (newCount == 0) {
             dispatchApplicationForegroundStatus(isInForeground = false)
         }
     }
 
     private fun dispatchApplicationForegroundStatus(isInForeground: Boolean) {
-        observers.forEach { observer ->
-            observer.onApplicationForegroundStatusChanged(isInForeground = isInForeground)
+        synchronized(observersLock) {
+            observers.forEach { observer ->
+                observer.onApplicationForegroundStatusChanged(isInForeground = isInForeground)
+            }
         }
     }
 }
