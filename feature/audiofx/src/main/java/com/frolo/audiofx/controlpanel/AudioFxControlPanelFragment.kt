4e5e1a430e334a5ec9eeca189ad3c2755f837081
Alexei <alexei.artsimovich@gmail.com>
2022-08-13 21:18:38 +0300

    fix

--- feature/audiofx/src/main/java/com/frolo/audiofx/controlpanel/AudioFxControlPanelFragment.kt
+++ feature/audiofx/src/main/java/com/frolo/audiofx/controlpanel/AudioFxControlPanelFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProviders
-import com.frolo.audiofx.R
+import com.frolo.audiofx.ui.R
 
 class AudioFxControlPanelFragment : Fragment() {
 
