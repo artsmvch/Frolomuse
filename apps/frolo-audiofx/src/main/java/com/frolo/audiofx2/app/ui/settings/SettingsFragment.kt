d9fd2e54131f32a289901e0f952584282d899843
Alexei <alexei.artsimovich@gmail.com>
2022-08-26 22:14:42 +0200

    created Settings fragment

---
+++ apps/frolo-audiofx/src/main/java/com/frolo/audiofx2/app/ui/settings/SettingsFragment.kt
@@ -0,0 +1,25 @@
+package com.frolo.audiofx2.app.ui.settings
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+import com.frolo.audiofx.app.R
+
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.app_preferences)
+        setupPreferences()
+    }
+
+    private fun setupPreferences() {
+        findPreference("theme")?.apply {
+            setOnPreferenceClickListener {
+                showThemeChooser()
+                true
+            }
+        }
+    }
+
+    private fun showThemeChooser() {
+
+    }
+}
