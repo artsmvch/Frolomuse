b640ce2202793231c057829d912583d3e9e2f4d0
Alexei <alexei.artsimovich@gmail.com>
2022-09-10 22:03:58 +0200

    added styleable for ReverbPanelView

--- feature/audiofx2/src/main/java/com/frolo/audiofx2/ui/view/ReverbPanelView.kt
+++ feature/audiofx2/src/main/java/com/frolo/audiofx2/ui/view/ReverbPanelView.kt
@@ -24,8 +24,8 @@ import kotlin.math.roundToInt
 class ReverbPanelView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
-): ConstraintLayout(context, attrs, defStyleAttr) {
+    defStyleAttr: Int = R.attr.reverbPanelViewStyle
+): ConstraintLayout(context, attrs, defStyleAttr, R.style.ReverbPanelView_Default) {
     // Views
     private val captionTextView: TextView
     private val enableStatusSwitch: SwitchMaterial
