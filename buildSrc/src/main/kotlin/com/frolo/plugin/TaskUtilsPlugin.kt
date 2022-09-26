3ac368af47e2dc3e91694f0e178f763185d2fe70
Alexei <alexei.artsimovich@gmail.com>
2022-09-26 22:12:32 +0200

    fixed method for collecting task dependencies

--- buildSrc/src/main/kotlin/com/frolo/plugin/TaskUtilsPlugin.kt
+++ buildSrc/src/main/kotlin/com/frolo/plugin/TaskUtilsPlugin.kt
@@ -1,10 +1,15 @@
 package com.frolo.plugin
 
 import org.gradle.api.*
+import org.gradle.api.internal.provider.ValueSupplier
 import org.gradle.api.provider.Property
+import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.TaskProvider
 import org.gradle.api.tasks.options.Option
+import java.io.PrintStream
+import java.util.*
 
 class TaskUtilsPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -28,16 +33,45 @@ internal abstract class PrintTaskDependenciesTask : DefaultTask(), Task {
             ?: throw GradleException("Task path is not specified")
         val task = project.tasks.findByPath(taskPath)
             ?: throw GradleException("Task $taskPath not found")
-        printTaskWithDependencies(task, 0)
+        printTaskWithDependencies(task, depth = 0, printStream = System.out)
     }
+}
 
-    private fun printTaskWithDependencies(task: Task, depth: Int) {
-        val tabs = "\t".repeat(depth)
-        println(tabs + task.path)
-        task.dependsOn.forEach { dependency ->
-            if (dependency is Task) {
-                printTaskWithDependencies(dependency, depth + 1)
-            }
+private fun printTaskWithDependencies(task: Task, depth: Int, printStream: PrintStream) {
+    val indent = " ".repeat(depth)
+    printStream.println(indent + task.name)
+    task.dependsOn.forEach { dep ->
+        collectTasksAsDep(task, dep).forEach { depTask ->
+            printTaskWithDependencies(depTask, depth = depth + 1, printStream)
         }
     }
+}
+
+private fun collectTasksAsDep(
+    task: Task,
+    dep: Any,
+): Collection<Task> {
+    val collection: Collection<Task>? = when {
+        dep is Task -> listOf(dep)
+        dep is String ->
+            listOf(task.project.tasks.getByPath(dep))
+        dep is TaskProvider<*> ->
+            listOf(dep.get())
+        dep is ValueSupplier -> {
+            val producerTasks = LinkedList<Task>()
+            dep.producer.visitContentProducerTasks {
+                producerTasks.add(this)
+            }
+            producerTasks
+        }
+        dep is Collection<*> -> {
+            dep.filterNotNull().flatMap { collectTasksAsDep(task, it) }
+        }
+        // Must go last
+        dep is Provider<*> && dep.isPresent -> {
+            (dep.get() as? Task)?.let(Collections::singleton)
+        }
+        else -> null
+    }
+    return collection ?: emptyList<Task>()
 }
