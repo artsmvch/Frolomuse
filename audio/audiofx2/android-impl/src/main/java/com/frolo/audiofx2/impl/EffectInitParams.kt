6c58ccd12f006c956a21d611b9a228a8aa2baad8
Alexei <alexei.artsimovich@gmail.com>
2022-11-17 20:04:56 +0100

    removed unused class

--- audio/audiofx2/android-impl/src/main/java/com/frolo/audiofx2/impl/EffectInitParams.kt
+++
@@ -1,9 +0,0 @@
-package com.frolo.audiofx2.impl
-
-import android.media.MediaPlayer
-
-internal data class EffectInitParams(
-    val priority: Int,
-    val audioSessionId: Int,
-    val mediaPlayer: MediaPlayer?
-)
