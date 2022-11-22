3fc9984b248b00138c72ebf30ec8a354266fee56
Alexei <alexei.artsimovich@gmail.com>
2022-11-22 19:52:25 +0100

    added implementor property to audio effect descriptor

--- audio/audiofx2/android-impl/src/main/java/com/frolo/audiofx2/impl/SimpleAudioEffectDescriptor.kt
+++ audio/audiofx2/android-impl/src/main/java/com/frolo/audiofx2/impl/SimpleAudioEffectDescriptor.kt
@@ -6,5 +6,6 @@ import com.frolo.audiofx2.AudioEffectDescriptor
 internal data class SimpleAudioEffectDescriptor(
     override val name: String,
     override val description: String? = null,
+    override val implementor: String? = null,
     override val warnings: List<AudioEffect2Warning> = emptyList()
 ) : AudioEffectDescriptor
