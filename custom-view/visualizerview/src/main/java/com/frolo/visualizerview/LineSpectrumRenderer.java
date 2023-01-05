9e604a8e9e60c290b946e347b783ffc9352d1443
alexei.artsimovich <alexei.artsimovich@gmail.com>
2023-01-05 20:19:34 +0100

    fixed LineSpectrum renderer

--- custom-view/visualizerview/src/main/java/com/frolo/visualizerview/LineSpectrumRenderer.java
+++ custom-view/visualizerview/src/main/java/com/frolo/visualizerview/LineSpectrumRenderer.java
@@ -15,19 +15,13 @@ public final class LineSpectrumRenderer extends TraceRenderer implements Visuali
     protected void renderTrace(@NonNull Canvas canvas, byte[] data, int traceIndex, @NonNull RenderParams params) {
         float barWidth = ((float) canvas.getWidth()) / params.count;
         float div = ((float) data.length) / params.count;
-        canvas.drawLine(0, canvas.getHeight() / 2f, canvas.getWidth(), canvas.getHeight() / 2f, params.paint);
         params.paint.setStrokeWidth(barWidth - params.gap);
 
         for (int i = 0; i < params.count; i++) {
-            int bytePosition = (int) Math.ceil(i * div);
-            int top = canvas.getHeight() / 2
-                    + (128 - Math.abs(data[bytePosition]))
-                    * (canvas.getHeight() / 2) / 128;
-
-            int bottom = canvas.getHeight() / 2
-                    - (128 - Math.abs(data[bytePosition]))
-                    * (canvas.getHeight() / 2) / 128;
-
+            int index = (int) Math.ceil(i * div);
+            int halfBarHeight = (128 - Math.abs(data[index])) * (canvas.getHeight() / 2) / 128;
+            int top = canvas.getHeight() / 2 + halfBarHeight;
+            int bottom = canvas.getHeight() / 2 - halfBarHeight;
             float barX = (i * barWidth) + (barWidth / 2);
             canvas.drawLine(barX, bottom, barX, canvas.getHeight() / 2f, params.paint);
             canvas.drawLine(barX, top, barX, canvas.getHeight() / 2f, params.paint);
