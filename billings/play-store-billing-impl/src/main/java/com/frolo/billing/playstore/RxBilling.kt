5a42bfefa7987caddd8f3b1a05b1d36717d0d41c
alexei.artsimovich <alexei.artsimovich@gmail.com>
2023-01-14 16:11:07 +0100

    refactored methods in RxBilling

--- billings/play-store-billing-impl/src/main/java/com/frolo/billing/playstore/RxBilling.kt
+++ billings/play-store-billing-impl/src/main/java/com/frolo/billing/playstore/RxBilling.kt
@@ -1,76 +1,17 @@
 package com.frolo.billing.playstore
 
-import android.content.Context
 import com.android.billingclient.api.*
 import io.reactivex.Scheduler
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposables
-import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-
-
-// TODO: refactor methods imn this file (DRY, visibility)
 
 private fun getMainThreadScheduler(): Scheduler {
     return AndroidSchedulers.mainThread()
 }
 
-private fun getQueryScheduler(): Scheduler {
-    return Schedulers.io()
-}
-
-/**
- * Returns an instance of [BillingClient] that is connected to the billing service and is ready for work.
- */
-fun prepareBillingClient(
-    context: Context,
-    purchasesUpdatedListener: PurchasesUpdatedListener
-): Single<BillingClient> {
-    val singleSource: Single<BillingClient> = Single.create { emitter ->
-
-        val client: BillingClient = BillingClient.newBuilder(context)
-            .enablePendingPurchases()
-            .setListener(purchasesUpdatedListener)
-            .build()
-
-        emitter.setDisposable(Disposables.fromAction {
-            //client.endConnection()
-        })
-
-        client.startConnection(object : BillingClientStateListener {
-            override fun onBillingSetupFinished(result: BillingResult) {
-                if (result.responseCode == BillingClient.BillingResponseCode.OK) {
-                    emitter.onSuccess(client)
-                } else {
-                    emitter.onError(BillingClientException(result))
-                }
-            }
-
-            override fun onBillingServiceDisconnected() {
-                emitter.onError(BillingClientException("Service was disconnected"))
-            }
-        })
-    }
-
-    val scheduler: Scheduler = getMainThreadScheduler()
-
-    return singleSource
-        .subscribeOn(scheduler)
-        .unsubscribeOn(scheduler)
-        .observeOn(scheduler)
-}
-
-/**
- * Returns an instance of [BillingClient] that is connected to the billing service and is ready for work.
- */
-fun prepareBillingClient(
-    context: Context,
-    onPurchasesUpdated: (result: BillingResult, purchases: List<Purchase>?) -> Unit
-): Single<BillingClient> = prepareBillingClient(context, PurchasesUpdatedListener(onPurchasesUpdated))
-
-fun BillingClient.querySkuDetailsSingle(skuList: List<String>, @BillingClient.SkuType type: String): Single<List<SkuDetails>> {
+internal fun BillingClient.querySkuDetailsSingle(skuList: List<String>, @BillingClient.SkuType type: String): Single<List<SkuDetails>> {
     val singleSource: Single<List<SkuDetails>> = Single.create { emitter ->
         val skuDetailsParams = SkuDetailsParams.newBuilder()
             .setSkusList(skuList)
@@ -89,41 +30,31 @@ fun BillingClient.querySkuDetailsSingle(skuList: List<String>, @BillingClient.Sk
             }
         }
     }
-
     val scheduler: Scheduler = getMainThreadScheduler()
-
     return singleSource
         .subscribeOn(scheduler)
         .unsubscribeOn(scheduler)
         .observeOn(scheduler)
 }
 
-fun BillingClient.queryPurchasesSingle(@BillingClient.SkuType type: String): Single<PurchasesResult> {
-    val source = Single.create<PurchasesResult> { emitter ->
-        GlobalScope.launch {
-            kotlin.runCatching {
-                queryPurchasesAsync(type)
-            }.onSuccess {
-                emitter.onSuccess(it)
-            }.onFailure {
-                emitter.onError(it)
-            }
-        }
-    }
-    return source
-        .subscribeOn(getQueryScheduler())
-        .unsubscribeOn(getMainThreadScheduler())
-        .observeOn(getMainThreadScheduler())
+internal fun BillingClient.queryPurchasesSingle(@BillingClient.SkuType type: String): Single<PurchasesResult> {
+    return coroutineToSingle { queryPurchasesAsync(type) }
 }
 
 internal fun BillingClient.queryPurchaseHistorySingle(@BillingClient.SkuType type: String): Single<PurchaseHistoryResult> {
-    val source = Single.create<PurchaseHistoryResult> { emitter ->
+    return coroutineToSingle {
+        val params = QueryPurchaseHistoryParams.newBuilder()
+            .setProductType(type)
+            .build()
+        queryPurchaseHistory(params)
+    }
+}
+
+private fun <T : Any> coroutineToSingle(callable: suspend () -> T): Single<T> {
+    val source = Single.create<T> { emitter ->
         GlobalScope.launch {
             kotlin.runCatching {
-                val params = QueryPurchaseHistoryParams.newBuilder()
-                    .setProductType(type)
-                    .build()
-                queryPurchaseHistory(params)
+                callable.invoke()
             }.onSuccess {
                 emitter.onSuccess(it)
             }.onFailure {
@@ -132,7 +63,7 @@ internal fun BillingClient.queryPurchaseHistorySingle(@BillingClient.SkuType typ
         }
     }
     return source
-        .subscribeOn(getQueryScheduler())
+        .subscribeOn(getMainThreadScheduler())
         .unsubscribeOn(getMainThreadScheduler())
         .observeOn(getMainThreadScheduler())
 }
