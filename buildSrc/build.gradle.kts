e2ddd56f8c345011c8c8be424bc42179b34fb454
Alexei <alexei.artsimovich@gmail.com>
2025-04-26 22:17:43 +0200

    Updated Android Gradle Plugin version to 8.9.2

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 
 dependencies {
     /* Depend on the android gradle plugin, since we want to access it in our plugin */
-    implementation("com.android.tools.build:gradle:7.1.3")
+    implementation("com.android.tools.build:gradle:8.9.2")
 
     /* Depend on the kotlin plugin, since we want to access it in our plugin */
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
